56f240bbeb2950e31ffe5ce66d5f5c54e699edf1
Abdumalik1912 <126488265+Abdumalik1912@users.noreply.github.com>
2023-10-14 15:58:17 +0500

    Update raceTime.py

--- raceTime.py
+++ raceTime.py
@@ -1,1 +1,10 @@
-print("hi")
+class Time:
+
+  def __init__(self, hours, minutes, seconds, milliseconds):
+    self._hours = hours
+    self._minutes = minutes
+    self._seconds = seconds
+    self._milliseconds = milliseconds
+
+  def to_string(self):
+    return f"{self._hours}:{self._minutes}:{self._seconds}:{self._milliseconds}"
