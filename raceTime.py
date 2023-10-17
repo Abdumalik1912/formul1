addf04a4e0063dddf14ba6004ac94da4d725eaac
Abdumalik Abdusalomov <aabdusalomov533@gmail.com>
2023-10-17 21:12:50 +0500

    Comitting indentation changes

--- raceTime.py
+++ raceTime.py
@@ -1,10 +1,11 @@
 class Time:
 
     def __init__(self, hours, minutes, seconds, milliseconds):
-      self._hours = hours
-      self._minutes = minutes
-      self._seconds = seconds
-      self._milliseconds = milliseconds
+        self._hours = hours
+        self._minutes = minutes
+        self._seconds = seconds
+        self._milliseconds = milliseconds
 
     def to_string(self):
-      return f"{self._hours}:{self._minutes}:{self._seconds}:{self._milliseconds}"
+        return f"{self._hours}:{self._minutes}:{self._seconds}:{self._milliseconds}"
+
