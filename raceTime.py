bc6bee87e381ffd110fcf5dc6c23964597b259f9
Abdumalik Abdusalomov <aabdusalomov533@gmail.com>
2023-10-17 20:49:52 +0500

    Committing again

--- raceTime.py
+++ raceTime.py
@@ -1,10 +1,10 @@
 class Time:
 
-  def __init__(self, hours, minutes, seconds, milliseconds):
-    self._hours = hours
-    self._minutes = minutes
-    self._seconds = seconds
-    self._milliseconds = milliseconds
+    def __init__(self, hours, minutes, seconds, milliseconds):
+      self._hours = hours
+      self._minutes = minutes
+      self._seconds = seconds
+      self._milliseconds = milliseconds
 
-  def to_string(self):
-    return f"{self._hours}:{self._minutes}:{self._seconds}:{self._milliseconds}"
+    def to_string(self):
+      return f"{self._hours}:{self._minutes}:{self._seconds}:{self._milliseconds}"
