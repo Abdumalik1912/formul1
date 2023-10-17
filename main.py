edd1d476fa5a8561a8bad23d36241031b3e3d435
Abdumalik1912 <126488265+Abdumalik1912@users.noreply.github.com>
2023-10-18 00:58:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ tournament = Championship()
 tournament.create_driver("Lewis Hamilton")
 tournament.create_driver("Michael Schumacher")
 tournament.create_driver("Sebastian Vettel")
-tournament.create_driver("Alain Rost")
-
 for d in tournament.get_drivers():
     print(d.get_name)
 
