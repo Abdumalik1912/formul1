6761098bf21647753adb1bc38f32ac87dfefe096
Abdumalik Abdusalomov <aabdusalomov533@gmail.com>
2023-10-19 16:40:14 +0500

    Adding enter_driver and get_drivers functions and creating new dict for storing driver data

--- gp.py
+++ gp.py
@@ -1,7 +1,12 @@
+from driver import Driver
+from raceTime import Time
+
+
 class GP:
 
     def __init__(self, name):
         self._name = name
+        self._drivers = {}
 
     @property
     def get_name(self):
@@ -10,3 +15,10 @@ class GP:
     @get_name.setter
     def get_name(self, new_name):
         self._name = new_name
+
+    def enter_driver(self, driver: Driver):
+        self._drivers.update({driver: [Time(00, 00, 00, 0000), 0]})
+
+    def get_drivers(self):
+        return self._drivers
+
