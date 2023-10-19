4c5c4ea472d5d37f5140b607a99954760c7b57aa
Abdumalik Abdusalomov <aabdusalomov533@gmail.com>
2023-10-19 15:09:21 +0500

    Using property in raceTime for encapsulation

--- raceTime.py
+++ raceTime.py
@@ -6,6 +6,30 @@ class Time:
         self._seconds = seconds
         self._milliseconds = milliseconds
 
+    @property
+    def get_hours(self):
+        return self._hours
+
+    @get_hours.setter
+    def get_hours(self, new_hours):
+        self._hours = new_hours
+
+    @property
+    def minutes(self):
+        return self._minutes
+
+    @minutes.setter
+    def minutes(self, new_minutes):
+        self._minutes = new_minutes
+
+    @property
+    def seconds(self):
+        return self._seconds
+
+    @property
+    def milliseconds(self):
+        return self._milliseconds
+
     def to_string(self):
         return f"{self._hours}:{self._minutes}:{self._seconds}:{self._milliseconds}"
 
